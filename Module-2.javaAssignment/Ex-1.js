1e69d80aec46cc6adcf98cb46e9ba22aa3115952
Thapafmly <111743779+Thapafmly@users.noreply.github.com>
2022-11-28 23:17:13 +0200

    Add files via upload

---
+++ Module-2.javaAssignment/Ex-1.js
@@ -0,0 +1,10 @@
+//Ex 1
+
+let input = []
+for (let i = 0; i < 5; i++){
+    input[i] = prompt(`Enter number: `);
+}
+
+for (let i = 4; i >= 0; i--){
+    console.log("Reversed order: " + input[i]);
+}
